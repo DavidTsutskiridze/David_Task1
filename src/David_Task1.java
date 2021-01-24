34441f95517ebbe3161f9be4596b17cef807387a
David Tsutskiridze <davidtsutskiridze@Davids-MacBook-Air.local>
2021-01-24 13:21:58 -0500

    second commit

--- src/David_Task1.java
+++ src/David_Task1.java
@@ -1,5 +1,9 @@
 public class David_Task1 {
     public static void main(String[] args) {
         System.out.println("tasks are very helpful");
+        String str = "car";
+
+        System.out.println("i like my car ");
+
     }
 }
