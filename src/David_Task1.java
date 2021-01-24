20a950825baf117ca40f9182cdf77990a673b3f6
David Tsutskiridze <davidtsutskiridze@Davids-MacBook-Air.local>
2021-01-24 13:23:33 -0500

    third one

--- src/David_Task1.java
+++ src/David_Task1.java
@@ -1,9 +1,12 @@
+import java.sql.SQLOutput;
+
 public class David_Task1 {
     public static void main(String[] args) {
         System.out.println("tasks are very helpful");
         String str = "car";
 
         System.out.println("i like my car ");
+        System.out.println("i got it");
 
     }
 }
